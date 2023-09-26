6b0a8775ba9667a82c9daad8fcf55bb385f31873
romangrbr <romangrbr@gmail.com>
2023-09-26 21:58:26 +0300

    ref

--- 1_tow_sum.py
+++ 1_tow_sum.py
@@ -1,2 +1,16 @@
+from typing import List
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        first: int = 0
+        while first < len(nums) - 1:
+            for second in range(first + 1, len(nums)):
+                if nums[first] + nums[second] == target:
+                    return [first, second]
+            first += 1
+
+
+if __name__ == '__main__':
+    result = Solution()
+    print(result.twoSum([3, 2, 3], 6))
